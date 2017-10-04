b0755eebdae9340fabc6679e4040a25db257c4ae
jforeman / Foreman <jforeman@eng.ucmerced.edu>
2017-10-04 00:29:51 +0000

    Add remaining files

---
+++ Hash.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include "Hash.h"
+
+using namespace std;
+
+
+/****************************************************************
+ * CONSTRUCTOR
+ ****************************************************************/
+Hash::Hash(int _bNo) {
+}
+
+/****************************************************************
+ * DESTRUCTOR
+ ****************************************************************/
+Hash::~Hash() {
+  // Write code to remove and deallocate everything
+}
+
+void Hash::Insert(int toInsert) {
+  // Write your code here
+}
+
+bool Hash::Delete(int toDelete) {
+  // Write your code here
+	return true;
+}
+
+bool Hash::Search(int key, int& _bucket, int& _pos) {
+  // Write your code here
+	return true;
+}
+
+void Hash::Print() {
+  // Write your code here
+}
+

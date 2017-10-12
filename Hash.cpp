c95033c36d64f13a59ad5010ca71794c7b9361f1
jforeman7 <31979772+jforeman7@users.noreply.github.com>
2017-10-11 19:44:14 -0700

    Update Hash.cpp

--- Hash.cpp
+++ Hash.cpp
@@ -13,8 +13,11 @@ using namespace std;
 // Constructor.
 Hash::Hash(int _bNo) 
 {
+	// Initialize the maximum amount of buckets.
 	this->bNo = _bNo;
-
+	
+	// Resize the table to reflect the maximum amount of buckets.
+	table.resize(_bNo);
 }
 
 // Destructor.
@@ -23,14 +26,38 @@ Hash::~Hash()
 	
 }
 
+// Function to insert keys into the hash table.
 void Hash::Insert(int toInsert) 
 {
+	// Calculate the hashed key: h(k) = k % m
+	// Where k is the key (toInsert) and m is the size of the chained table (bNo).
+	int hashKey = toInsert % bNo;
 	
+	// Insert into the table.
+	table[hashKey].push_front(toInsert);
 }
 
+// Function to delete keys in the hash table.
+// It must first find the key, then delete it.
+// If it doesn't find the key, then it returns false.
 bool Hash::Delete(int toDelete) 
 {
-	return true;
+	// Calculate the hashd key.
+	int hashKey = toDelete % bNo;
+	
+	// Iterator for moving through the vector portion of the table.
+	int i = 0;
+	
+	// STL iterator for moving through the list portion of the table.
+	for (list<int>::const_iterator j = table[hashKey].begin(); j != table[hashKey].end(); j++)
+	{
+		if(*j == toDelete)
+		{
+			table[hashKey].erase(j);
+			return true;
+		}
+	}
+	return false;
 }
 
 bool Hash::Search(int key, int& _bucket, int& _pos) 
